6af427e9f5e9540b315889a4580f8a41e5b639c6
catborn <itnoz@yandex.ru>
2024-09-03 20:07:00 +0300

    add data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-function App() {
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 8],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Learn frontend", 2],
+  ["Sleep", 7.5],
+  ["Play with a cat", 1.5],
+];
+
+export const options = {
+  title: "My Happy Daily Activities",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>hi</p>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
-
-export default App;
